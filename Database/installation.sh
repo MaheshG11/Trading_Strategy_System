aac7574bacc9d1cff0114542a73222787d90df33
Mahesh Ghumare <76564249+MaheshG11@users.noreply.github.com>
2024-03-27 19:03:07 +0530

    Delete Database/installation.sh

--- Database/installation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo service mysql start
-mysql Schema.sql
-mysql sqlusersetup.sql
-# RUN sudo mysql
-# RUN ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Ghumare123@';
-# RUN exit
-
